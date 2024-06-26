d168e495cd1fd35006ccdb047e56a5a35245a795
Trijeet Modak <uniquetrij@gmail.com>
2024-06-26 15:29:52 +0530

    refactored schema

--- ai/schema.py
+++ ai/schema.py
@@ -20,176 +20,204 @@ def instruction_schema(**members: dict[str, dict[str, ...]]):
         "type": "object",
         "description": env.schema.PRIMARY_INSTRUCTION,
         "properties": {
-            "ocr": {
-                "type": "string",
-                "description": env.schema.OCR_INSTRUCTION,
-            },
-            "meta": {
-                "type": "object",
-                "description": env.schema.META_INSTRUCTION,
-                "properties": __format_schema_properties(env.schema.META_INDICATORS),
-            },
-            "health": {
-                "type": "object",
-                "description": env.schema.HEALTH_INSTRUCTION,
-                "properties": {
-                    "report": {
+            **__ocr(),
+            **__meta(),
+            **__health(),
+            **__product(members)
+        }
+    }
+
+
+def __health():
+    return {
+        "health": {
+            "type": "object",
+            "description": env.schema.HEALTH_INSTRUCTION,
+            "properties": {
+                "report": {
+                    "type": "object",
+                    "description": env.schema.HEALTH_REPORT_INSTRUCTION,
+                    "properties": {
+                        **__format_schema_properties(env.schema.HEALTH_REPORT_INDICATORS)
+                    }
+                },
+                "demography": {
+                    "type": "object",
+                    "description": env.schema.HEALTH_DEMOGRAPTHY_INSTRUCTION,
+                    "properties": {
+                        **__format_schema_properties(env.schema.HEALTH_DEMOGRAPTHY_INDICATORS)
+                    }
+                },
+                "vitals": {
+                    "type": "object",
+                    "description": env.schema.HEALTH_VITALS_INSTRUCTION,
+                    "properties": {
+                        **__format_schema_properties(env.schema.HEALTH_VITALS_INDICATORS)
+                    }
+                },
+                "issues": {
+                    "type": "array",
+                    "description": env.schema.HEALTH_CONCERNS_INSTRUCTION,
+                    "items": {
                         "type": "object",
-                        "description": env.schema.HEALTH_REPORT_INSTRUCTION,
+                        "description": env.schema.HEALTH_CONCERNS_ITEMS_INSTRUCTION,
                         "properties": {
-                            **__format_schema_properties(env.schema.HEALTH_REPORT_INDICATORS)
-                        }
-                    },
-                    "demography": {
-                        "type": "object",
-                        "description": env.schema.HEALTH_DEMOGRAPTHY_INSTRUCTION,
-                        "properties": {
-                            **__format_schema_properties(env.schema.HEALTH_DEMOGRAPTHY_INDICATORS)
-                        }
-                    },
-                    "vitals": {
-                        "type": "object",
-                        "description": env.schema.HEALTH_VITALS_INSTRUCTION,
-                        "properties": {
-                            **__format_schema_properties(env.schema.HEALTH_VITALS_INDICATORS)
-                        }
-                    },
-                    "concerns": {
-                        "type": "array",
-                        "description": env.schema.HEALTH_CONCERNS_INSTRUCTION,
-                        "items": {
-                            "type": "object",
-                            "description": env.schema.HEALTH_CONCERNS_ITEMS_INSTRUCTION,
-                            "properties": {
-                                **__format_schema_properties(env.schema.HEALTH_CONCERNS_ITEMS_INDICATORS),
-                                "precautions": {
-                                    "type": "array",
-                                    "description": env.schema.HEALTH_CONCERNS_ITEMS_PRECAUTIONS_INSTRUCTION,
-                                    "items": {
-                                        "type": "object",
-                                        "description": env.schema.HEALTH_CONCERNS_ITEMS_PRECAUTIONS_INDICATORS_INSTRUCTION,
-                                        "properties": {
-                                            **__format_schema_properties(
-                                                env.schema.HEALTH_CONCERNS_INGREDIENT_INDICATORS)
-                                        }
+                            **__format_schema_properties(env.schema.HEALTH_CONCERNS_ITEMS_INDICATORS),
+                            "precautions": {
+                                "type": "array",
+                                "description": env.schema.HEALTH_CONCERNS_ITEMS_PRECAUTIONS_INSTRUCTION,
+                                "items": {
+                                    "type": "object",
+                                    "description": env.schema.HEALTH_CONCERNS_ITEMS_PRECAUTIONS_INDICATORS_INSTRUCTION,
+                                    "properties": {
+                                        **__format_schema_properties(
+                                            env.schema.HEALTH_CONCERNS_INGREDIENT_INDICATORS)
                                     }
                                 }
                             }
                         }
                     }
                 }
-            },
-            "product": {
-                "type": "object",
-                "description": env.schema.PRODUCT_INSTRUCTION,
-                "properties": {
-                    "specific": {
-                        "type": "object",
-                        "description": env.schema.PRODUCT_SPECIFIC_INSTRUCTION,
-                        "properties": {
-                            member: {
-                                "type": "object",
-                                "description": env.schema.PRODUCT_SPECIFIC_MEMBER_INSTRUCTION.format(member=member,
-                                                                                                     vitals=vitals,
-                                                                                                     concerns=concerns),
-                                "properties": {
-                                    concern: {
-                                        "type": "object",
-                                        "description": env.schema.PRODUCT_SPECIFIC_MEMBER_CONCERN_INSTRUCTION.format(
-                                            member=member, concern=concern),
-                                        "properties": {
-                                            "constituents": {
-                                                "type": "array",
-                                                "description": env.schema.CONSTITUENTS_INSTRUCTION.format(
+            }
+        }
+    }
+
+
+def __product(members):
+    return {
+        "product": {
+            "type": "object",
+            "description": env.schema.PRODUCT_INSTRUCTION,
+            "properties": {
+                "specific": {
+                    "type": "object",
+                    "description": env.schema.PRODUCT_SPECIFIC_INSTRUCTION,
+                    "properties": {
+                        member: {
+                            "type": "object",
+                            "description": env.schema.PRODUCT_SPECIFIC_MEMBER_INSTRUCTION.format(member=member,
+                                                                                                 vitals=vitals,
+                                                                                                 concerns=concerns),
+                            "properties": {
+                                concern: {
+                                    "type": "object",
+                                    "description": env.schema.PRODUCT_SPECIFIC_MEMBER_CONCERN_INSTRUCTION.format(
+                                        member=member, concern=concern),
+                                    "properties": {
+                                        "constituents": {
+                                            "type": "array",
+                                            "description": env.schema.CONSTITUENTS_INSTRUCTION.format(
+                                                concern=concern),
+                                            "items": {
+                                                "type": "object",
+                                                "description": env.schema.CONSTITUENTS_ITEM_INSTRUCTION.format(
                                                     concern=concern),
-                                                "items": {
-                                                    "type": "object",
-                                                    "description": env.schema.CONSTITUENTS_ITEM_INSTRUCTION.format(
-                                                        concern=concern),
-                                                    "properties": __format_schema_properties(
-                                                        env.schema.CONSTITUENTS_ITEM_INDICATORS,
-                                                        member=member,
-                                                        concern=concern
-                                                    )
-                                                }
-                                            },
-                                            **(
-                                                __format_schema_properties(
-                                                    env.schema.PRODUCT_ITEM_INDICATORS,
-                                                    concern=concern, member=member)
-                                            )
-                                        }
-                                    } for concern, level in concerns.items()
-                                }
-                            } for member, (v, c) in members.items() if
-                            (vitals := members[member][v]) | (concerns := members[member][c])
-                        }
-                    },
-                    "generic": {
-                        "type": "object",
-                        "description": env.schema.PRODUCT_GENERIC_INSTRUCTION,
-                        "properties": {
-                            'allergens': {
-                                "type": "array",
-                                "description": env.schema.PRODUCT_ALLERGENS_INSTRUCTION,
-                                "items": {
-                                    "type": "object",
-                                    "description": env.schema.PRODUCT_ALLERGENS_ITEM_INSTRUCTION,
-                                    "properties": {
-                                        "name": {
-                                            "type": "string"
+                                                "properties": __format_schema_properties(
+                                                    env.schema.CONSTITUENTS_ITEM_INDICATORS,
+                                                    member=member,
+                                                    concern=concern
+                                                )
+                                            }
                                         },
-                                        "proportion": {
-                                            "type": "number"
-                                        },
-                                        "justification": {
-                                            "type": "string"
-                                        }
+                                        **(
+                                            __format_schema_properties(
+                                                env.schema.PRODUCT_ITEM_INDICATORS,
+                                                concern=concern, member=member)
+                                        )
+                                    }
+                                } for concern, level in concerns.items()
+                            }
+                        } for member, (v, c) in members.items() if
+                        (vitals := members[member][v]) | (concerns := members[member][c])
+                    }
+                },
+                "generic": {
+                    "type": "object",
+                    "description": env.schema.PRODUCT_GENERIC_INSTRUCTION,
+                    "properties": {
+                        'allergens': {
+                            "type": "array",
+                            "description": env.schema.PRODUCT_ALLERGENS_INSTRUCTION,
+                            "items": {
+                                "type": "object",
+                                "description": env.schema.PRODUCT_ALLERGENS_ITEM_INSTRUCTION,
+                                "properties": {
+                                    "name": {
+                                        "type": "string"
+                                    },
+                                    "proportion": {
+                                        "type": "number"
+                                    },
+                                    "justification": {
+                                        "type": "string"
                                     }
                                 }
-                            },
-                            'acutes': {
-                                "type": "array",
-                                "description": env.schema.PRODUCT_ACUTES_INSTRUCTION,
-                                "items": {
-                                    "type": "object",
-                                    "description": env.schema.PRODUCT_ACUTES_ITEM_INSTRUCTION,
-                                    "properties": {
-                                        "name": {
-                                            "type": "string"
-                                        },
-                                        "proportion": {
-                                            "type": "number"
-                                        },
-                                        "justification": {
-                                            "type": "string"
-                                        }
+                            }
+                        },
+                        'acutes': {
+                            "type": "array",
+                            "description": env.schema.PRODUCT_ACUTES_INSTRUCTION,
+                            "items": {
+                                "type": "object",
+                                "description": env.schema.PRODUCT_ACUTES_ITEM_INSTRUCTION,
+                                "properties": {
+                                    "name": {
+                                        "type": "string"
+                                    },
+                                    "proportion": {
+                                        "type": "number"
+                                    },
+                                    "justification": {
+                                        "type": "string"
                                     }
                                 }
-                            },
-                            'chronics': {
-                                "type": "array",
-                                "description": env.schema.PRODUCT_CHRONICS_INSTRUCTION,
-                                "items": {
-                                    "type": "object",
-                                    "description": env.schema.PRODUCT_CHRONICS_ITEM_INSTRUCTION,
-                                    "properties": {
-                                        "name": {
-                                            "type": "string"
-                                        },
-                                        "proportion": {
-                                            "type": "number"
-                                        },
-                                        "justification": {
-                                            "type": "string"
-                                        }
+                            }
+                        },
+                        'chronics': {
+                            "type": "array",
+                            "description": env.schema.PRODUCT_CHRONICS_INSTRUCTION,
+                            "items": {
+                                "type": "object",
+                                "description": env.schema.PRODUCT_CHRONICS_ITEM_INSTRUCTION,
+                                "properties": {
+                                    "name": {
+                                        "type": "string"
+                                    },
+                                    "proportion": {
+                                        "type": "number"
+                                    },
+                                    "justification": {
+                                        "type": "string"
                                     }
                                 }
                             }
                         }
-                    },
-                }
+                    }
+                },
+                "verdict": {
+                    "type": "string",
+                    "description": "Your final verdict whether I should purchase this product or not considering the health issues and concerns or my family members."
+                },
             }
+        }
+    }
+
+
+def __meta():
+    return {
+        "meta": {
+            "type": "object",
+            "description": env.schema.META_INSTRUCTION,
+            "properties": __format_schema_properties(env.schema.META_INDICATORS),
+        }
+    }
+
+
+def __ocr():
+    return {
+        "ocr": {
+            "type": "string",
+            "description": env.schema.OCR_INSTRUCTION,
         }
     }
 
